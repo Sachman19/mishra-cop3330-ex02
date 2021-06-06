4c2d2648e566bd76f45d7e10f370fff7c98445d4
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 01:08:15 -0400

    Version 1 Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your input string? ");
+        String inString = input.nextLine();
+
+        System.out.printf("%s has length of %d.", inString, inString.length());
     }
 }
